ccac6f71b094ebc3999abf15b30feaf429e94c43
Mariah Lightfoot <mariahlightfoot@Mariahs-MacBook-Pro.local>
2019-11-23 09:16:56 -0600

    trying to complete the bonus. need to think. leaving bad code in here

--- lesson2/examples/input-practice/src/app/movie-list/movie-list.component.ts
+++ lesson2/examples/input-practice/src/app/movie-list/movie-list.component.ts
@@ -14,6 +14,8 @@ export class MovieListComponent implements OnInit {
    }
 
    addMovie(newTitle: string){
-      this.movies.push(newTitle);
+      if(!this.movies.includes(newTitle) && newTitle !== ""){
+         this.movies.push(newTitle);
+      }
    }
 }
